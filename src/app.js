741df48b2b41593f283c8e24bdf84f2b4f7d297f
Dhruv Marwaha <dhruv.marwaha03@gmail.com>
2025-02-03 02:25:13 +0530

    add comment, like, playlist, subscription, tweet, video, dashboard, and health check controllers and routes

--- src/app.js
+++ src/app.js
@@ -15,6 +15,14 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 import userRouter from "./routes/user.routes.js";
+// import healthcheckRouter from "./routes/healthcheck.routes.js"
+// import tweetRouter from "./routes/tweet.routes.js"
+// import subscriptionRouter from "./routes/subscription.routes.js"
+// import videoRouter from "./routes/video.routes.js"
+// import commentRouter from "./routes/comment.routes.js"
+// import likeRouter from "./routes/like.routes.js"
+// import playlistRouter from "./routes/playlist.routes.js"
+// import dashboardRouter from "./routes/dashboard.routes.js"
 
 app.use("/api/v1/users",userRouter)
 
